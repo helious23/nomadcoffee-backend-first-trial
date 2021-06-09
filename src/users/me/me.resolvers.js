64d36b6b6693c1a9f09fddeacce3ef7f88efeca6
Max16 <max16@naver.com>
2021-06-09 17:24:26 +0900

    Query me return

--- src/users/me/me.resolvers.js
+++ src/users/me/me.resolvers.js
@@ -3,12 +3,12 @@ import { protectedResolver } from "../users.utils";
 
 export default {
   Query: {
-    me: protectedResolver((_, __, { loggedInUser }) => {
-      return client.user.findUnique({
+    me: protectedResolver((_, __, { loggedInUser }) =>
+      client.user.findUnique({
         where: {
           id: loggedInUser.id,
         },
-      });
-    }),
+      })
+    ),
   },
 };
